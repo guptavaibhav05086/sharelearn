63a86613e52125a36bddf0bb181f4081ac22737b
guptavaibhav.05086@gmail.com <guptavaibhav.05086@gmail>
2020-07-20 21:35:52 +0530

    cart functonality

--- src/app/mainsitecomponents/order-cart/order-cart.component.ts
+++ src/app/mainsitecomponents/order-cart/order-cart.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-order-cart',
-  templateUrl: './order-cart.component.html',
-  styleUrls: ['./order-cart.component.css']
+  selector: "app-order-cart",
+  templateUrl: "./order-cart.component.html",
+  styleUrls: ["./order-cart.component.css"]
 })
 export class OrderCartComponent implements OnInit {
-
-  constructor() { }
+  userCart = {
+    designNprint: [],
+    design: [],
+    print: [],
+    displayPrint: false,
+    displayDesign: false,
+    displayDesignNPrint: false,
+    finalPrice: {
+      calPrice: 0,
+      calDelivery: 0,
+      calGST: 0,
+      calFinalTotal: 0
+    },
+    totalItems:0
+  };
+  constructor() {}
 
   ngOnInit(): void {
+    this.loadCart();
   }
 
+  loadCart() {
+    debugger;
+    let cartItems = JSON.parse(localStorage.getItem("cart"));
+    this.userCart.totalItems=cartItems.length;
+    if (cartItems != null && cartItems.length > 0) {
+      cartItems.forEach(item => {
+        if (item.type == "Design And Print") {
+          this.userCart.designNprint.push(item.category[0]);
+          this.userCart.finalPrice.calPrice += item.category[0].price.price;
+          this.userCart.finalPrice.calGST += item.category[0].price.GST;
+          this.userCart.finalPrice.calDelivery +=
+            item.category[0].price.deliveryFee;
+          this.userCart.finalPrice.calFinalTotal +=
+            item.category[0].price.Total;
+        } else if (item.type == "Design Only") {
+          console.log(item.category[0]);
+          this.userCart.design.push(item.category[0]);
+          this.userCart.finalPrice.calPrice += item.category[0].price.price;
+          this.userCart.finalPrice.calGST += item.category[0].price.GST;
+          this.userCart.finalPrice.calDelivery +=
+            item.category[0].price.deliveryFee;
+          this.userCart.finalPrice.calFinalTotal +=
+            item.category[0].price.Total;
+        } else if (item.type == "Print Only") {
+          this.userCart.print.push(item.category[0]);
+          this.userCart.finalPrice.calPrice += item.category[0].price.price;
+          this.userCart.finalPrice.calGST += item.category[0].price.GST;
+          this.userCart.finalPrice.calDelivery +=
+            item.category[0].price.deliveryFee;
+          this.userCart.finalPrice.calFinalTotal +=
+            item.category[0].price.Total;
+          //return ;
+        }
+      });
+
+      if (this.userCart.designNprint.length > 0) {
+        this.userCart.displayDesignNPrint = true;
+      }
+      // cartItems.forEach(item => {
+      //   if (item.type == "Design Only") {
+      //     console.log(item.category[0]);
+      //     this.userCart.design.push(item.category[0]);
+      //   }
+      // });
+
+      if (this.userCart.design.length > 0) {
+        this.userCart.displayDesign = true;
+      }
+      // cartItems.forEach(item => {
+      //   if (item.type == "Print Only") {
+      //     this.userCart.print.push(item.category[0]);
+      //     //return ;
+      //   }
+      // });
+
+      if (this.userCart.print.length > 0) {
+        this.userCart.displayPrint = true;
+      }
+    }
+    console.log(this.userCart);
+  }
 }
--- src/app/mainsitecomponents/review-order/review-order.component.ts
+++ src/app/mainsitecomponents/review-order/review-order.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review-order.component.css']
 })
 export class ReviewOrderComponent implements OnInit {
-
+  userCart = {
+    designNprint: [],
+    design: [],
+    print: [],
+    displayPrint: false,
+    displayDesign: false,
+    displayDesignNPrint: false,
+    finalPrice: {
+      calPrice: 0,
+      calDelivery: 0,
+      calGST: 0,
+      calFinalTotal: 0
+    },
+    totalItems:0
+  };
   toggleDisplayItems=false;
   name="Show Products"
   toggleItems(event){
@@ -25,6 +39,57 @@ export class ReviewOrderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.loadCart();
   }
+  loadCart() {
+    debugger;
+    let cartItems = JSON.parse(localStorage.getItem("cart"));
+    this.userCart.totalItems=cartItems.length;
+    if (cartItems != null && cartItems.length > 0) {
+      cartItems.forEach(item => {
+        if (item.type == "Design And Print") {
+          this.userCart.designNprint.push(item.category[0]);
+          this.userCart.finalPrice.calPrice += item.category[0].price.price;
+          this.userCart.finalPrice.calGST += item.category[0].price.GST;
+          this.userCart.finalPrice.calDelivery +=
+            item.category[0].price.deliveryFee;
+          this.userCart.finalPrice.calFinalTotal +=
+            item.category[0].price.Total;
+        } else if (item.type == "Design Only") {
+          console.log(item.category[0]);
+          this.userCart.design.push(item.category[0]);
+          this.userCart.finalPrice.calPrice += item.category[0].price.price;
+          this.userCart.finalPrice.calGST += item.category[0].price.GST;
+          this.userCart.finalPrice.calDelivery +=
+            item.category[0].price.deliveryFee;
+          this.userCart.finalPrice.calFinalTotal +=
+            item.category[0].price.Total;
+        } else if (item.type == "Print Only") {
+          this.userCart.print.push(item.category[0]);
+          this.userCart.finalPrice.calPrice += item.category[0].price.price;
+          this.userCart.finalPrice.calGST += item.category[0].price.GST;
+          this.userCart.finalPrice.calDelivery +=
+            item.category[0].price.deliveryFee;
+          this.userCart.finalPrice.calFinalTotal +=
+            item.category[0].price.Total;
+          //return ;
+        }
+      });
 
+      if (this.userCart.designNprint.length > 0) {
+        this.userCart.displayDesignNPrint = true;
+      }
+      
+
+      if (this.userCart.design.length > 0) {
+        this.userCart.displayDesign = true;
+      }
+     
+
+      if (this.userCart.print.length > 0) {
+        this.userCart.displayPrint = true;
+      }
+    }
+    console.log(this.userCart);
+  }
 }
